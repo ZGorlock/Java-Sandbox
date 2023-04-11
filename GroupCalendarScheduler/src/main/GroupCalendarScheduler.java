29672074be3af0fb840dcdb69bab02eec170e152
Zachary Gill <zachary.w.gill@gmail.com>
2023-04-11 07:05:04 -0400

    Updated GroupCalendarScheduler

--- GroupCalendarScheduler/src/main/GroupCalendarScheduler.java
+++ GroupCalendarScheduler/src/main/GroupCalendarScheduler.java
@@ -9,11 +9,21 @@ package main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Files;
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
@@ -43,6 +53,18 @@ public class GroupCalendarScheduler {
     
     private static File CALENDARS_FILE = new File("data/calendars.txt");
     
+    private static String TIMEZONE_OFFSET = "-04";
+    
+    private static ZoneId ZONE_ID = ZoneOffset.ofHours(Integer.parseInt(TIMEZONE_OFFSET));
+    
+    private static final int INTERVAL = 30;
+    
+    private static final long INTERVAL_MS = TimeUnit.MINUTES.toMillis(INTERVAL);
+    
+    private static final int INTERVALS_PER_HOUR = (int) TimeUnit.HOURS.toMinutes(1) / INTERVAL;
+    
+    private static final int INTERVALS_PER_DAY = (int) TimeUnit.DAYS.toMinutes(1) / INTERVAL;
+    
     
     //Static Fields
     
@@ -52,28 +74,74 @@ public class GroupCalendarScheduler {
     
     private static Map<String, List<Event>> events;
     
-    private static String startTime = "2023-04-01";
+    private static List<EventRange> eventRanges;
     
-    private static String endTime = "2023-05-01";
+    private static List<Slot> availableSlots;
+    
+    private static final int minimumSlot = 4 * INTERVALS_PER_HOUR; //4 hours
+    
+    private static final List<DayOfWeek> daysAllowed = List.of(
+            DayOfWeek.SUNDAY
+//            , DayOfWeek.MONDAY
+//            , DayOfWeek.TUESDAY
+//            , DayOfWeek.WEDNESDAY
+//            , DayOfWeek.THURSDAY
+            , DayOfWeek.FRIDAY
+            , DayOfWeek.SATURDAY
+    );
+    
+    private static final int earliestTime = 9 * INTERVALS_PER_HOUR; //9:00 AM
+    
+    private static final int latestTime = 22 * INTERVALS_PER_HOUR; //10:00 PM
+    
+    private static final String startDate = "2023-04-01";
+    
+    private static final String endDate = "2023-05-01";
+    
+    private static final DateTime startTime = DateTime.parseRfc3339(startDate + "T00:00:00" + TIMEZONE_OFFSET + ":00");
+    
+    private static final DateTime endTime = DateTime.parseRfc3339(endDate + "T00:00:00" + TIMEZONE_OFFSET + ":00");
     
     
     //Main Method
     
     public static void main(String[] args) throws Exception {
-        initService();
+        init();
         loadCalendars();
         loadEvents();
         
-        parseEvents();
+        calculateEventRanges();
+        findAvailableSlots();
         
-        System.out.println("Done");
+        print();
     }
     
     
     //Static Methods
     
-    private static void parseEvents() {
-        //TODO
+    private static Calendar init() throws Exception {
+        service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, authorize())
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        return service;
+    }
+    
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    private static Credential authorize() throws Exception {
+        final GoogleCredential serviceKey = GoogleCredential
+                .fromStream(new FileInputStream(SERVICE_ACCOUNT_KEY_FILE))
+                .createScoped(SCOPES);
+
+//        final GoogleCredential credential = new GoogleCredential.Builder()
+//                .setTransport(readJsonFile.getTransport())
+//                .setJsonFactory(readJsonFile.getJsonFactory())
+//                .setServiceAccountId(readJsonFile.getServiceAccountId())
+////                .setServiceAccountUser("zgorlock@gmail.com")
+//                .setServiceAccountScopes(readJsonFile.getServiceAccountScopes())
+//                .setServiceAccountPrivateKey(readJsonFile.getServiceAccountPrivateKey())
+//                .build();
+        
+        return serviceKey;
     }
     
     private static Map<String, List<String>> loadCalendars() throws Exception {
@@ -100,7 +168,7 @@ public class GroupCalendarScheduler {
         
         for (List<String> userCalendars : calendars.values()) {
             for (String calendarId : userCalendars) {
-                final List<Event> calendarEvents = getCalendarEvents(calendarId);
+                final List<Event> calendarEvents = getCalendarEvents(calendarId, true);
                 
                 events.putIfAbsent(calendarId, new ArrayList<>());
                 events.get(calendarId).addAll(calendarEvents);
@@ -110,22 +178,24 @@ public class GroupCalendarScheduler {
         return events;
     }
     
-    private static List<Event> getCalendarEvents(String calendarId) {
+    private static List<Event> getCalendarEvents(String calendarId, boolean getInstances) {
         final List<Event> events = new ArrayList<>();
         
         String pageToken = null;
         do {
             try {
-                final Events eventData = service.events().list(calendarId)
-                        .setTimeMin(DateTime.parseRfc3339(startTime + "T00:00:00-04:00"))
-                        .setTimeMax(DateTime.parseRfc3339(endTime + "T00:00:00-04:00"))
-                        .setPageToken(pageToken)
-                        .setMaxResults(50)
+                final Events eventsResponse = service.events().list(calendarId)
+                        .setTimeMin(startTime).setTimeMax(endTime)
+                        .setPageToken(pageToken).setMaxResults(2500)
                         .execute();
                 
-                events.addAll(eventData.getItems());
+                eventsResponse.getItems().stream()
+                        .map(e -> (!getInstances || (e.getRecurrence() == null)) ? List.of(e) :
+                                getEventInstances(calendarId, e.getId()))
+                        .flatMap(Collection::stream)
+                        .forEachOrdered(events::add);
                 
-                pageToken = eventData.getNextPageToken();
+                pageToken = eventsResponse.getNextPageToken();
             } catch (Exception e) {
                 e.printStackTrace();
                 throw new RuntimeException(e);
@@ -135,29 +205,243 @@ public class GroupCalendarScheduler {
         return events;
     }
     
-    private static Calendar initService() throws Exception {
-        service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, authorize())
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-        return service;
+    private static List<Event> getCalendarEvents(String calendarId) {
+        return getCalendarEvents(calendarId, false);
     }
     
-    @SuppressWarnings("UnnecessaryLocalVariable")
-    private static Credential authorize() throws Exception {
-        final GoogleCredential serviceKey = GoogleCredential
-                .fromStream(new FileInputStream(SERVICE_ACCOUNT_KEY_FILE))
-                .createScoped(SCOPES);
-
-//        final GoogleCredential credential = new GoogleCredential.Builder()
-//                .setTransport(readJsonFile.getTransport())
-//                .setJsonFactory(readJsonFile.getJsonFactory())
-//                .setServiceAccountId(readJsonFile.getServiceAccountId())
-////                .setServiceAccountUser("zgorlock@gmail.com")
-//                .setServiceAccountScopes(readJsonFile.getServiceAccountScopes())
-//                .setServiceAccountPrivateKey(readJsonFile.getServiceAccountPrivateKey())
-//                .build();
+    private static List<Event> getEventInstances(String calendarId, String eventId) {
+        final List<Event> instances = new ArrayList<>();
         
-        return serviceKey;
+        String pageToken = null;
+        do {
+            try {
+                final Events instancesResponse = service.events().instances(calendarId, eventId)
+                        .setTimeMin(startTime).setTimeMax(endTime)
+                        .setPageToken(pageToken).setMaxResults(2500)
+                        .execute();
+                
+                instances.addAll(instancesResponse.getItems());
+                
+                pageToken = instancesResponse.getNextPageToken();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        } while (pageToken != null);
+        
+        return instances;
+    }
+    
+    private static List<EventRange> calculateEventRanges() {
+        eventRanges = new ArrayList<>();
+        
+        for (List<Event> calendarEvents : events.values()) {
+            for (Event event : calendarEvents) {
+                final EventRange eventRange = new EventRange(event);
+                
+                eventRanges.add(eventRange);
+            }
+        }
+        
+        return eventRanges;
+    }
+    
+    private static List<Slot> findAvailableSlots() {
+        availableSlots = new ArrayList<>();
+        
+        Slot currentSlot = null;
+        for (long pointer = startTime.getValue(); pointer < endTime.getValue(); pointer += INTERVAL_MS) {
+            final DateTime point = new DateTime(pointer);
+            final Slot interval = new Slot(point);
+            
+            if (interval.isValid() && interval.isAvailable()) {
+                if (currentSlot != null) {
+                    currentSlot.extend(interval);
+                } else {
+                    currentSlot = interval;
+                }
+            } else {
+                if (currentSlot != null) {
+                    if (currentSlot.isValidLength()) {
+                        availableSlots.add(currentSlot);
+                    }
+                    currentSlot = null;
+                }
+            }
+        }
+        if ((currentSlot != null) && currentSlot.isValidLength()) {
+            availableSlots.add(currentSlot);
+        }
+        
+        return availableSlots;
+    }
+    
+    private static void print() {
+        final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("EEE MM/dd/yyyy").withZone(ZONE_ID);
+        final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm").withZone(ZONE_ID);
+        
+        System.out.println("┌" + "─".repeat(48) + "┐");
+        
+        for (Slot slot : availableSlots) {
+            final Instant start = Instant.ofEpochMilli(slot.start.getValue());
+            final Instant end = Instant.ofEpochMilli(slot.end.getValue());
+            
+            System.out.printf("│ %s   |   %s -> %s   |  %4.1fh │%n",
+                    dateFormat.format(start), timeFormat.format(start), timeFormat.format(end), slot.getHours());
+        }
+        
+        System.out.println("└" + "─".repeat(48) + "┘");
+    }
+    
+    
+    //Inner Classes
+    
+    private static class Interval {
+        
+        //Fields
+        
+        public DateTime start;
+        
+        public DateTime end;
+        
+        
+        //Constructors
+        
+        public Interval(DateTime start, DateTime end) {
+            this.start = start;
+            this.end = end;
+        }
+        
+        
+        //Methods
+        
+        public void extend(DateTime time) {
+            this.start = startsAfter(time) ? time : this.start;
+            this.end = endsBefore(time) ? time : this.end;
+        }
+        
+        public void extend(Interval interval) {
+            if (!touches(interval)) {
+                throw new UnsupportedOperationException();
+            }
+            extend(interval.start);
+            extend(interval.end);
+        }
+        
+        public boolean encloses(DateTime time) {
+            return (start.getValue() < time.getValue()) &&
+                    (end.getValue() > time.getValue());
+        }
+        
+        public boolean encloses(Interval interval) {
+            return encloses(interval.start) && encloses(interval.end);
+        }
+        
+        public boolean overlaps(Interval interval) {
+            return encloses(interval.start) || encloses(interval.end);
+        }
+        
+        public boolean touches(Interval interval) {
+            return (start.getValue() == interval.end.getValue()) ||
+                    (end.getValue() == interval.start.getValue());
+        }
+        
+        public boolean endsBefore(DateTime time) {
+            return (end.getValue() <= time.getValue());
+        }
+        
+        public boolean endsBefore(Interval interval) {
+            return endsBefore(interval.start);
+        }
+        
+        public boolean startsAfter(DateTime time) {
+            return (start.getValue() >= time.getValue());
+        }
+        
+        public boolean startsAfter(Interval interval) {
+            return startsAfter(interval.end);
+        }
+        
+    }
+    
+    private static class Slot extends Interval {
+        
+        //Constructors
+        
+        public Slot(DateTime start) {
+            super(start, new DateTime(start.getValue() + INTERVAL_MS));
+        }
+        
+        
+        //Methods
+        
+        private boolean isTimeValid(DateTime test, boolean start) {
+            final Instant instant = Instant.ofEpochMilli(test.getValue());
+            final ZonedDateTime time = ZonedDateTime.ofInstant(instant, ZONE_ID);
+            final Instant startOfDay = time.plus((start ? 1 : -1), ChronoUnit.MILLIS).toLocalDate().atStartOfDay(ZONE_ID).toInstant();
+            final Instant earliest = startOfDay.plus((earliestTime * INTERVAL_MS), ChronoUnit.MILLIS);
+            final Instant latest = startOfDay.plus((latestTime * INTERVAL_MS), ChronoUnit.MILLIS);
+            
+            return daysAllowed.contains(time.getDayOfWeek()) &&
+                    !instant.isBefore(earliest) && !instant.isAfter(latest);
+        }
+        
+        public boolean isStartTimeValid() {
+            return isTimeValid(start, true);
+        }
+        
+        public boolean isEndTimeValid() {
+            return isTimeValid(end, false);
+        }
+        
+        public boolean isValid() {
+            return isStartTimeValid() && isEndTimeValid();
+        }
+        
+        public boolean isValidLength() {
+            return getDuration() >= (minimumSlot * INTERVAL_MS);
+        }
+        
+        public boolean isAvailable() {
+            return eventRanges.stream().noneMatch(e -> e.blocks(this));
+        }
+        
+        public long getDuration() {
+            return end.getValue() - start.getValue();
+        }
+        
+        public double getHours() {
+            return getDuration() / (double) TimeUnit.HOURS.toMillis(1);
+        }
+        
+    }
+    
+    private static class EventRange extends Interval {
+        
+        //Fields
+        
+        public final Event event;
+        
+        public final boolean busy;
+        
+        
+        //Constructors
+        
+        public EventRange(Event event) {
+            super(Optional.ofNullable(event).map(Event::getStart).map(e -> e.containsKey("dateTime") ? e.getDateTime() : e.getDate()).orElse(null),
+                    Optional.ofNullable(event).map(Event::getEnd).map(e -> e.containsKey("dateTime") ? e.getDateTime() : e.getDate()).orElse(null));
+            
+            this.event = event;
+            this.busy = Optional.ofNullable(event).map(Event::getTransparency).map(e -> !e.equalsIgnoreCase("transparent")).orElse(true);
+        }
+        
+        
+        //Methods
+        
+        public boolean blocks(Interval interval) {
+            return busy && overlaps(interval);
+        }
+        
     }
     
 }

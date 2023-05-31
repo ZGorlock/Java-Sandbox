bc128c0664f2d36173b70caf17d38df01f785a95
Zachary Gill <zachary.w.gill@gmail.com>
2023-05-31 06:47:32 -0400

    Added PadUtil

---
+++ PadUtil/src/main/PadUtil.java
@@ -0,0 +1,34 @@
+/*
+ * File:    PadUtil.java
+ * Package: main
+ * Author:  Zachary Gill
+ */
+
+package main;
+
+import main.data.cache.DataMirror;
+import main.data.parser.DataParser;
+
+public class PadUtil {
+    
+    //Constants
+    
+    public static final boolean MIRROR = false;
+    
+    public static final boolean PARSE = false;
+    
+    
+    //Main Method
+    
+    public static void main(String[] args) {
+        if (MIRROR) {
+            DataMirror.sync();
+        }
+        
+        if (PARSE) {
+            DataParser.parse();
+        }
+        
+    }
+    
+}
---
+++ PadUtil/src/main/data/parser/DataParser.java
@@ -0,0 +1,24 @@
+/*
+ * File:    DataParser.java
+ * Package: main.data.parser
+ * Author:  Zachary Gill
+ */
+
+package main.data.parser;
+
+public final class DataParser {
+    
+    //Main Methods
+    
+    public static void main(String[] args) {
+        parse();
+    }
+    
+    
+    //Static Methods
+    
+    public static void parse() {
+        
+    }
+    
+}
